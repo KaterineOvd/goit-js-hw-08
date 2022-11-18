0acd4a6dc668a63cf68c98c6ccb4c4facd98a6d8
KaterineOvd <kat.ovdienko@gmail.com>
2022-11-18 23:22:46 +0200

    tast 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = 'videoplayer-current-time';
+
+
+function onPlay(time) {
+  localStorage.setItem(KEY, time.seconds);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(KEY));
+
 
